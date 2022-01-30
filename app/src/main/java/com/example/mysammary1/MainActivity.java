b1e4779b4ff7760c88a69b94e11107ea5f034471
trying-man <zealousness@naver.com>
2022-01-30 14:27:44 +0900

    likeCount step1

--- app/src/main/java/com/example/mysammary1/MainActivity.java
+++ app/src/main/java/com/example/mysammary1/MainActivity.java
@@ -8,9 +8,27 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /*CommnetAdapter adapter;*/
+
+    Button likeButton;
+    Button UnlikeButton;
+    TextView likeCountView;
+    TextView UnlikeCountView;
+
+    int likeCount =0;
+    boolean likeState = false;
+
+    int UnlikeCount =0;
+    boolean UnlikeState = false;
+
+    EditText editText;
+    EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +39,43 @@ public class MainActivity extends AppCompatActivity {
 
         String content = function_text.getText().toString();
         SpannableString spannableString = new SpannableString(content);
+
+        likeButton = (Button) findViewById(R.id.likeButton);
+        likeCountView = (TextView) findViewById(R.id.likeCountView);
+
+        UnlikeButton = (Button) findViewById(R.id.UnlikeButton);
+        UnlikeCountView = (TextView) findViewById(R.id.UnlikeCountView);
+
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(likeState){
+                    decrLikeCount();
+                }else {
+                    incrLikeCount();
+                }
+                likeState = ! likeState;
+            }
+        });
+
+        UnlikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(UnlikeState){
+                    decrUnLikeCount();
+                }else if(likeState ==true &&UnlikeState ==false){
+                    decrLikeCount();
+                    incrUnLikeCount();
+
+                    UnlikeState = ! UnlikeState;
+                }else {
+                    incrUnLikeCount();
+
+                }
+
+            }
+        });
+
 
 
 
@@ -43,4 +98,35 @@ public class MainActivity extends AppCompatActivity {
 
         function_text.setText(spannableString);
     }
+
+    public void incrLikeCount(){
+        likeCount +=1;
+        likeCountView.setText(String.valueOf(likeCount));
+
+        likeButton.setBackgroundResource(R.drawable.ic_thumb_up_selected);
+    }
+
+    public void decrLikeCount(){
+        likeCount -=1;
+        likeCountView.setText(String.valueOf(likeCount));
+
+        likeButton.setBackgroundResource(R.drawable.thumbs_up_selector);
+    }
+
+    public void incrUnLikeCount(){
+        UnlikeCount +=1;
+        UnlikeCountView.setText(String.valueOf(UnlikeCount));
+
+        UnlikeButton.setBackgroundResource(R.drawable.ic_thumb_up_selected);
+    }
+
+    public void decrUnLikeCount(){
+        UnlikeCount -=1;
+        UnlikeCountView.setText(String.valueOf(UnlikeCount));
+
+        UnlikeButton.setBackgroundResource(R.drawable.thumbs_up_selector);
+
+    }
+
+
 }
